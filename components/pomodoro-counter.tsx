57455da068f1ae4658c62130921d49a8cb1a5afe
Cauã Mattos <caua.h4k@protonmail.com>
2025-03-25 13:19:29 -0300

    feat: increased margin y in the pomodoro counter

--- components/pomodoro-counter.tsx
+++ components/pomodoro-counter.tsx
@@ -4,7 +4,7 @@ import { Button } from "./button";
 const PomodoroCounter = () => {
     return (
         // main container
-        <div className="flex flex-col w-full text-center justify-center space-y-2 my-16">
+        <div className="flex flex-col w-full text-center justify-center space-y-2 my-32">
             {/* pomodoro title */}
             <div className="font-bold text-xl text-title text-center w-full ">
                 <span>Pronto para mais uma sessão de foco?</span>
