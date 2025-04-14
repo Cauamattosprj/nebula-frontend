4172fb12ce5cfe9bea55f27639c8721c0eb3cf2e
Caua Mattos - Casa <caua.h4k@gmail.com>
2025-04-14 15:26:44 -0300

    fix: fixed the note type

--- types/note.ts
+++ types/note.ts
@@ -10,7 +10,14 @@ export interface Note {
 }
 
 export interface NoteApiResponse {
-    data: [Note];
+    data: Note[];
+    message: string;
+    success: boolean;
+    statusCode: number;
+}
+
+export interface SingleNoteApiResponse {
+    data: Note | undefined;
     message: string;
     success: boolean;
     statusCode: number;
