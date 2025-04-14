04379f0424e74009456eda5bf8d139271b305b17
Caua Mattos - Casa <caua.h4k@gmail.com>
2025-04-14 15:23:55 -0300

    feat: changed notebook body to display real titles and bodys. Also implemented a custom hook with useEffect for resizing the title textarea

--- components/notebook/notebook-body.tsx
+++ components/notebook/notebook-body.tsx
@@ -1,22 +1,46 @@
 "use client";
 
 import { useCurrentOpenNoteStore } from "@/store/note";
+import { useEffect, useRef } from "react";
 
 const NotebookBody = () => {
-    const id = useCurrentOpenNoteStore((state) => state.currentOpenNoteId);
+    const currentOpenNote = useCurrentOpenNoteStore(
+        (state) => state.currentOpenNote
+    );
+
+    const titleRef = useRef<HTMLTextAreaElement>(null);
+
+    // hooks for auto resizing
+    useEffect(() => {
+        const el = titleRef.current;
+        if (el) {
+            el.style.height = "auto";
+            el.style.height = el.scrollHeight + "px";
+        }
+    }, [currentOpenNote?.title]);
+    const handleTitleInput = () => {
+        const el = titleRef.current;
+        if (el) {
+            el.style.height = "auto";
+            el.style.height = el.scrollHeight + "px";
+        }
+    };
 
     return (
-        <div className="px-12 py-8 flex flex-col space-y-8 min-h-screen">
+        // TODO: Define the correct height for this container based on the number of items in the notebook sidebar
+        <div className="px-12 py-8 flex flex-col space-y-8 h-[3000px]">
             {/* note title */}
-            <input
-                className="note-title text-title text-4xl font-bold folder-title text-left glowing bg-transparent focus:outline-none focus:border-none focus:ring-0"
-                defaultValue={"Herança"}
+            <textarea
+                ref={titleRef}
+                onInput={handleTitleInput}
+                className="note-title text-title text-4xl w-full font-bold text-left bg-transparent focus:outline-none resize-none overflow-hidden"
+                defaultValue={currentOpenNote?.title ?? ""}
+                rows={1}
             />
+
             {/* note body */}
             <textarea
-                name=""
-                id=""
-                value={id || ""}
+                defaultValue={currentOpenNote?.body ?? ""}
                 className="note-body text-paragraph min-h-screen bg-transparent focus:border-none focus:outline-none focus:ring-0"
             />
         </div>
