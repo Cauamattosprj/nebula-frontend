4981b18ef688de9b65e363043e0d12f6228c8f6f
Caua Mattos - Casa <caua.h4k@gmail.com>
2025-04-26 16:54:23 -0300

    fix: fixed updateNoteById function with object parameters and headers

--- lib/api/services/notes/update-note-by-id.ts
+++ lib/api/services/notes/update-note-by-id.ts
@@ -1,15 +1,25 @@
 import { SingleNoteApiResponse } from "@/types/note";
 import { UUID } from "crypto";
 
-export const updateNoteById = async (id?: UUID, title?: string, body?: string, folderId?: UUID) => {
+interface updateNoteByIdProps {
+    id?: UUID,
+    title?: string,
+    body?: string,
+    folderId?: UUID
+}
+
+export const updateNoteById = async ({ id, title, body, folderId }: updateNoteByIdProps) => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/notes/update/${id}`, {
-        method: "POST",
+        method: "PUT",
         mode: "cors",
         body: JSON.stringify({
             title,
+            folderId,
             body,
-            folderId
-        })
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
     })
 
     if (!response.ok) {
